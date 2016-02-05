d6f97a638f9bfcbe935541537402581afcf346a7
Gosha Arinich <me@goshakkk.name>
2016-02-05 23:41:51 +0300

    Move app component

--- app/initialize.js
+++ app/initialize.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from 'app';
+import App from 'components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('app'));
