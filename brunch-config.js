3192874181763dcd23ce5c9424528c101fe071d8
Paul Miller <paul@paulmillr.com>
2017-02-06 16:02:05 +0200

    Improve config.

--- brunch-config.js
+++ brunch-config.js
@@ -1,15 +1,13 @@
-module.exports = {
-  files: {
-    javascripts: {
-      joinTo: {
-        'vendor.js': /^(?!app)/,
-        'app.js': /^app/
-      }
-    },
-    stylesheets: {joinTo: 'app.css'}
+exports.files =
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^(?!app)/,
+      'app.js': /^app/
+    }
   },
+  stylesheets: {joinTo: 'app.css'}
+};
 
-  plugins: {
-    babel: {presets: ['es2015', 'react']}
-  }
+exports.plugins =
+  babel: {presets: ['es2015', 'react']}
 };
