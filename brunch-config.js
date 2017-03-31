9939c92bf5918f268dbf87df4bfcacfdf94cf75b
Adrian Unger <dev@staydecent.ca>
2017-03-31 15:01:41 -0700

    Add new deps and linting.

--- brunch-config.js
+++ brunch-config.js
@@ -7,8 +7,24 @@ exports.files = {
     }
   },
   stylesheets: {joinTo: 'app.css'}
-};
+}
 
 exports.plugins = {
-  babel: {presets: ['latest', 'react']}
-};
+  babel: {
+    presets: ['env', 'react'],
+    plugins: [
+      ['babel-plugin-module-resolver', {
+        'alias': {
+          // This will cause require paths starting with `/` to resolve to the
+          // `src` directory. i.e. `/app.js` resolves to `src/app.js`.
+          '': './src'
+        }
+      }],
+      ['./node_modules/babel-plugin-jsx-import/src/index.js', {
+        'identifier': 'Preact',
+        'moduleName': 'preact'
+      }],
+      ['transform-react-jsx', {'pragma': 'Preact.h'}]
+    ]
+  }
+}
