451174428fe02ebac75ea3decf7a93604b60b29d
Adrian Unger <dev@staydecent.ca>
2017-07-12 22:55:26 -0700

    Update Page.

--- app/components/elements/page/index.js
+++ app/components/elements/page/index.js
@@ -1,37 +1,6 @@
-
-import {css} from 'glamor'
-
-const pageContainer = css({
-  display: 'block',
-  boxSizing: 'border-box',
-  width: '100%',
-  height: '100%',
-  overflowY: 'auto'
-})
-
-const slideIn = css.keyframes({
-  '0%': { transform: 'translateX(10%)', opacity: 0 },
-  '100%': { transform: 'translateX(0)', opacity: 1 }
-})
-
-const slideInFromLeft = css.keyframes({
-  '0%': { transform: 'translateX(-10%)', opacity: 0 },
-  '100%': { transform: 'translateX(0)', opacity: 1 }
-})
-
-const pageStyle = (noPadding = false, back) => css({
-  display: 'block',
-  boxSizing: 'border-box',
-  margin: '0px auto',
-  width: '100%',
-  maxWidth: '1200px',
-  padding: noPadding ? '0' : '1em 2em 2em 2em',
-  animation: `${back ? slideInFromLeft : slideIn} .5s`
-})
-
-export const Page = ({back, noPadding, children}) =>
-  <div {...pageContainer}>
-    <div {...pageStyle(noPadding, back)}>
+export const Page = ({back, className, children}) =>
+  <div class='page-container'>
+    <div class={`page ${className || ''}${back ? ' back' : ' forward'}`}>
       {children}
     </div>
   </div>
